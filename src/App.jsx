515f5eb43de4563b075bbf5f8c95358e0201bc1d
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-09-01 11:38:53 +0500

    frame add layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Typography, Card } from "@material-tailwind/react";
+import Frame from "./components/Frame";
 
 export default function App() {
   return (
     <div>
-      
+      <Frame/>
     </div>
   );
 }
---
+++ src/components/Frame.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../layouts/Header'
+
+const Frame = () => {
+  return (
+    <div className='w-full max-w-[1920px] mx-auto px-5'>
+        <Header/>
+    </div>
+  )
+}
+
+export default Frame
