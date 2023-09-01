114868dc56b2c494cf4ecd2e92f6c0bcb0685326
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-09-01 11:47:25 +0500

    fontFamily add

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-        
+        <div className='w-full max-w-[1920px] mx-auto px-5'>
+            <div>
+                <div>
+                    <a href="#">
+                        <h1 className=' uppercase'>akstar</h1>
+                    </a>
+                </div>
+            </div>
+        </div>
     </header>
   )
 }
