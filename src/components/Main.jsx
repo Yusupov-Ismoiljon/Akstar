aa257a75b4511eda34856cf48d9326ad74b222e1
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-09-30 22:21:41 +0500

    I deleted the toggleInfo main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,44 +15,34 @@ const Main = ({ addCart, addCart1, addLike, removeLike, e }) => {
     }
   };
 
-  const toggleInfo = () => {
-    setShowInfo(!showInfo);
-  };
-
   return (
+    // cart
     <li
       key={e.id}
       data-aos-delay="100"
       data-aos="fade-up"
-      className="show text-white relative aos-init aos-animate rounded-lg p-5"
+      className="w-full show text-white relative aos-init aos-animate rounded-lg p-3"
     >
+      <div>
+        <img className="w-full h-72 rounded-lg brightness-90" src={e.img} alt="new product" />
+        <button onClick={() => toggleLike(e.id)} className="absolute w-5 h-5 top-3 right-3 z-30">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" id="like"><path fill={`${liked ? 'red' : 'white'}`} d="M11.692 1C10.123 1 8.753 1.946 8 3.182 7.247 1.946 5.877 1 4.308 1 1.928 1 0 2.899 0 5.242c0 1.173.468 2.246 1.231 3.031C2.963 10.054 8 15 8 15s5.037-4.946 6.769-6.727A4.341 4.341 0 0 0 16 5.242C16 2.899 14.072 1 11.692 1z"></path></svg>
+        </button>
 
-      <Link onClick={() => addCart1(e)} className="bg-white rounded-xl py-4" to={`/guitar/${e.id}`}>
-          <img className="w-full" src={e.img} alt="new product" />
-      </Link>
+      </div>
       <div className="py-3 space-y-1">
         <span className="font-medium text-sm text-[#DEDEDE]">{e.ak}</span>
         <h3 className="font-bold text-sm sm:text-base brend">{e.lc}</h3>
-        <span className="font-extrabold text-base sm:text-lg logo">{e.pul} ₽</span>
+        <span className="font-extrabold text-base sm:text-lg logo">{e.pul}  ₽</span>
       </div>
-      <button onClick={() => toggleLike(e.id)} className="absolute top-3 right-3 z-30">
-        <i
-          className={`fa-sharp fa-solid text-${liked ? 'red-500' : '[#B0B0B0]'} fa-heart fa-xl`}
-          aria-hidden="true"
-        ></i>
-      </button>
-      <button onClick={() => toggleInfo()} className="absolute top-3 left-3 z-30">
-        <i className={`fa-sharp fa-info-circle fa-xl`} aria-hidden="true"></i>
-      </button>
-      {showInfo && (
-        <div className="absolute top-20 left-3 z-50 bg-white p-4 rounded-md shadow-md">
-          {/* Display your information here */}
-          <p>This is the information you want to show when the button is clicked.</p>
-        </div>
-      )}
-      <button onClick={() => addCart(e.id)} className="font-semibold text-base leading-6 text-[#1B37A3] py-1 px-9 border-2 border-[#1B37A3] rounded-md">
-        {e.karzinka}
-      </button>
+      <div className="flex">
+        <button onClick={() => addCart(e.id)} className="w-full font-semibold text-base leading-6 text-[#1B37A3] py-1 px-9 border-2 border-[#1B37A3] rounded-md mr-2">
+          {e.karzinka}
+        </button>
+        <Link onClick={() => addCart1(e.id)} to={`/guitar/${e.id}`} className="flex justify-center items-center px-5 font-semibold text-base leading-6  text-white bg-[#1B37A3] rounded-md">
+          i
+        </Link>
+      </div>
     </li>
   );
 };
