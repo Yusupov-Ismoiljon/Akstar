6a0d2c699f3f2031525319a2d8b0f594497fc10e
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-04 04:19:06 +0500

    remov img, add text

--- src/components/Tavar.jsx
+++ src/components/Tavar.jsx
@@ -12,35 +12,8 @@ const Tavar = () => {
             <button data-aos="fade-up" className="bg-white text-black rounded-[10px] text-lg font-semibold md:py-4 md:px-9 py-2 px-5 aos-init aos-animate">Перейти в каталог</button>
           </div>
           <div>
-            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
-              <li data-aos-delay="0" data-aos="fade-up" className="text-white relative aos-init aos-animate">
-                <div>
-                  <img className="w-full h-64 rounded-lg" src="./img/erkak1.jpeg" alt="new product" />
-                </div>
-                <div className="py-3">
-                  <h3 className="font-bold text-sm sm:text-base">Мужская футболка Street Style</h3>
-                  <span className="font-extrabold text-base sm:text-lg">711₽</span>
-                </div>
-              </li>
-              <li data-aos-delay="300" data-aos="fade-up" className="text-white relative aos-init aos-animate">
-                <div>
-                  <img className="w-full h-64 rounded-lg" src="./img/ayolar3.jpg" alt="new product" />
-                </div>
-                <div className="py-3">
-                  <h3 className="font-semibold text-sm sm:text-base">Куртка Anaki черная 2091 женская</h3>
-                  <span className="font-bold text-base sm:text-lg">609 ₽</span>
-                </div>
-              </li>
-              <li data-aos-delay="600" data-aos="fade-up" className="text-white relative aos-init aos-animate">
-                <div>
-                  <img className="w-full h-64 rounded-lg" src="./img/bola1.jpg" alt="new product" />
-                </div>
-                <div className="py-3">
-                  <h3 className="font-semibold text-sm sm:text-base">Джинсы для мальчиков, Rumino Jeans</h3>
-                  <span className="font-bold text-base sm:text-lg">517 ₽</span>
-                </div>
-              </li>
-            </ul>
+            <p className='font-bold text-3xl text-white'>Новое изделие поступит на завод в течение 1 месяца.</p>
+            <p className='font-bold text-3xl text-white'>Мы продаем ткань</p>
           </div>
         </div>
       </div>
