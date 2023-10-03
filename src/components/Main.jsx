356925af138811c2247112555f6e16a6f38638cb
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-03 22:11:48 +0500

    unnecessary deletion

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -30,14 +30,14 @@ const Main = ({ addCart, addLike, removeLike, e }) => {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" id="like"><path fill={`${liked ? 'red' : 'white'}`} d="M11.692 1C10.123 1 8.753 1.946 8 3.182 7.247 1.946 5.877 1 4.308 1 1.928 1 0 2.899 0 5.242c0 1.173.468 2.246 1.231 3.031C2.963 10.054 8 15 8 15s5.037-4.946 6.769-6.727A4.341 4.341 0 0 0 16 5.242C16 2.899 14.072 1 11.692 1z"></path></svg>
         </button>
       </div>
-      {/*  */}
+      
       <div className="grow rounded-md p-2.5">
         <h3 className="font-bold text-sm sm:text-base brend">{e.lc}</h3>
         <span className="font-extrabold text-base sm:text-lg logo">{e.pul} ₽</span>
       </div>
-      {/*  */}
+      {/* karzinka */}
       <div>
-        {/* link  */}
+        {/* link */}
         <div className="flex items-center p-2.5 pt-0">
           <button onClick={() => addCart(e.id)} className="w-full font-semibold text-base leading-6 text-[#1B37A3] py-1 px-9 border-2 border-[#1B37A3] rounded-md mr-2">
             {e.karzinka}
