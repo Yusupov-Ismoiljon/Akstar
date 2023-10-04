ef4c12b49e8ba176b029b0ef877f7d46306d1025
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-04 15:13:53 +0500

    add img and div

--- src/components/Tavar.jsx
+++ src/components/Tavar.jsx
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
 
 const Tavar = () => {
-  const [isLiked, setIsLiked] = useState(false);
 
   return (
     <section className="bg-[#1B37A3] py-7">
       <div className="w-full max-w-[1300px] mx-auto px-5">
-        <div className="lg:flex justify-between gap-y-8 items-center">
-          <div className="lg:mb-0 mb-5 text-white">
-            <h2 data-aos="fade-right" className="font-bold md:text-5xl text-2xl mb-[30px] block aos-init aos-animate">Поступление новых товаров</h2>
-            <button data-aos="fade-up" className="bg-white text-black rounded-[10px] text-lg font-semibold md:py-4 md:px-9 py-2 px-5 aos-init aos-animate">Перейти в каталог</button>
-          </div>
+        <div className='flex items-center justify-between'>
           <div>
-            <p className='font-bold text-3xl text-white'>Новое изделие поступит на завод в течение 1 месяца.</p>
-            <p className='font-bold text-3xl text-white'>Мы продаем ткань</p>
+            <div className="lg:mb-0 mb-5 text-white">
+              <h2 data-aos="fade-right" className="font-bold md:text-3xl text-xl mb-[30px] block aos-init aos-animate">Поступление новых товаров</h2>
+            </div>
+            <div>
+              <p data-aos="fade-right" className='font-bold text-xl text-white aos-init aos-animate'>Новое изделие поступит на завод в течение 1 месяца.</p>
+              <p data-aos="fade-right" className='font-bold text-xl text-white aos-init aos-animate'>Мы продаем ткань</p>
+            </div>
           </div>
+          <img data-aos="fade-left" className='w-[500px] h-[400px] rounded-xl aos-init aos-animate' src="./img/erkak12.jpeg" alt="" />
         </div>
+      </div>
+      <div>
       </div>
     </section>
   )
