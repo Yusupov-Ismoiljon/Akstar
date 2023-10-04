5c33e0683b4dae2a6c215b9235d48150b83cbcf3
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-04 17:20:11 +0500

    add aos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="md:flex md:justify-between gap-7">
           <div className='mb-7 md:mb-0'>
             <Link to={`/`}>
-              <h1 data-aos-delay="100" data-aos="fade-up" className='font-russo font-light md:text-[40.34px] text-[23px] text-white leading-[135%] uppercase mb-7  aos-init aos-animate'>akstar</h1>
+              <h1 data-aos-delay="100" data-aos="fade-up" className='font-russo font-light md:text-[40.34px] text-[23px] text-white leading-[135%] uppercase mb-7 aos-init aos-animate'>akstar</h1>
             </Link>
             <div>
               <ul className='flex space-x-3'>
@@ -45,7 +45,7 @@ const Footer = () => {
               </ul>
             </div>
           </div>
-          <div className='mb-7 md:mb-0'>
+          <div data-aos-delay="100" data-aos="fade-up" className='mb-7 md:mb-0 aos-init aos-animate'>
             <h3 className='font-bold text-2xl leading-normal mb-7'>Меню</h3>
             <ul className='space-y-5'>
               <li>
@@ -62,10 +62,10 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div>
+          <div data-aos-delay="100" data-aos="fade-up" className='aos-init aos-animate'>
             <div className='flex flex-col space-y-4 mb-7'>
-              <span data-aos-delay="100" data-aos="fade-up" className='font-bold text-2xl leading-normal aos-init aos-animate'>Служба поддержки</span>
-              <div data-aos-delay="100" data-aos="fade-up" className='aos-init aos-animate'>
+              <span className='font-bold text-2xl leading-normal'>Служба поддержки</span>
+              <div>
                 <a className='block mb-2.5' href="tel:+7 (499) 286 87 10">+7 (499) 286 87 10</a>
                 <a target="_blank" href="https://akstar.help@gmail.com">akstar.help@gmail.com</a>
               </div>
