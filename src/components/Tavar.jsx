0ca6b118c1bcd18e5537c560bcec0af1f51ee9d0
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-04 17:28:56 +0500

    add responsive

--- src/components/Tavar.jsx
+++ src/components/Tavar.jsx
@@ -5,8 +5,8 @@ const Tavar = () => {
   return (
     <section className="bg-[#1B37A3] py-7">
       <div className="w-full max-w-[1300px] mx-auto px-5">
-        <div className='flex items-center justify-between'>
-          <div>
+        <div className='md:flex md:items-center md:justify-between'>
+          <div className='mb-5 md:mb-0 lg:mb-0 xl:mb-0'>
             <div className="lg:mb-0 mb-5 text-white">
               <h2 data-aos="fade-right" className="font-bold md:text-3xl text-xl mb-[30px] block aos-init aos-animate">Поступление новых товаров</h2>
             </div>
@@ -15,7 +15,7 @@ const Tavar = () => {
               <p data-aos="fade-right" className='font-bold text-xl text-white aos-init aos-animate'>Мы продаем ткань</p>
             </div>
           </div>
-          <img data-aos="fade-left" className='w-[500px] h-[400px] rounded-xl aos-init aos-animate' src="./img/erkak12.jpeg" alt="" />
+          <img data-aos="fade-left" className='md:w-[500px] md:h-[400px] sm:w-[200px] sm:h-[200px] w-full h-[300px] rounded-xl aos-init aos-animate' src="./img/erkak12.jpeg" alt="" />
         </div>
       </div>
       <div>
