489f77559ab1dd766675a91863208825cb4594e5
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-05 19:44:48 +0500

    add padding

--- src/components/Magazin.jsx
+++ src/components/Magazin.jsx
@@ -3,7 +3,7 @@ import Header from './Header';
 import Footer from './Footer';
 import { toast } from 'react-toastify';
 
-const Magazin = ({ shop, setShop, addCartPul}) => {
+const Magazin = ({ shop, setShop, addCartPul }) => {
   const [pul, setPul] = useState(true);
   const [namber, setNamber] = useState(1);
   const [narhi, setNarhi] = useState(0);
@@ -45,10 +45,10 @@ const Magazin = ({ shop, setShop, addCartPul}) => {
           <div className={`xl:flex xl:justify-between gap-7 ${shop.length >= 0 ? 'block' : 'hidden'}`}>
             <ul className='flex flex-col gap-5 space-y-3'>
               {shop.map((product) => (
-                <li className='flex flex-col gap-7' key={product.id}>
+                <li className='flex flex-col show rounded-lg gap-7' key={product.id}>
                   <div className='sm:flex sm:justify-between w-full gap-[30px] items-start'>
-                    <img className='h-[190px] rounded-md' src={product.img} alt='product img' />
-                    <div>
+                    <img className='w-full h-[300px] rounded-md' src={product.img} alt='product img' />
+                    <div className='p-3'>
                       <div className='flex justify-between gap-7 mb-7'>
                         <div>
                           <p className='font-bold text-2xl leading-normal mb-4'>{product.lc}</p>
@@ -78,28 +78,28 @@ const Magazin = ({ shop, setShop, addCartPul}) => {
                           </div>
                         </div>
                       </div>
+                      <div className='flex justify-between items-center xl:hidden block p-3'>
+                        <p className='font-bold text-3xl logo leading-normal'>{product.pul} ₽</p>
+
+                        <div className='space-x-4'>
+                          <button onClick={() => addCartPul(product, -1)} disabled={product.miqdor <= 1}>
+                            <i className='fa-solid fa-minus'></i>
+                          </button>
+                          <span className='font-semibold text-xl leading-normal'>{product.miqdor}</span>
+                          <button onClick={() => addCartPul(product, 1)}>
+                            <i className='fa-solid fa-plus'></i>
+                          </button>
+                        </div>
+                        <button className='hover:text-red-500 duration-500' onClick={() => deleteCard(product.id)}>
+                          <span className='text-[#B0B0B0]'>
+                            Удалить
+                            <i className='fa-solid fa-trash-can ml-2'></i>
+                          </span>
+                        </button>
+                      </div>
                     </div>
                   </div>
 
-                  <div className='flex justify-between items-center xl:hidden block'>
-                    <p className='font-bold text-3xl logo leading-normal'>{product.pul} ₽</p>
-
-                    <div className='space-x-4'>
-                      <button onClick={() => addCartPul(product, -1)} disabled={product.miqdor <= 1}>
-                        <i className='fa-solid fa-minus'></i>
-                      </button>
-                      <span className='font-semibold text-xl leading-normal'>{product.miqdor}</span>
-                      <button onClick={() => addCartPul(product, 1)}>
-                        <i className='fa-solid fa-plus'></i>
-                      </button>
-                    </div>
-                    <button className='hover:text-red-500 duration-500' onClick={() => deleteCard(product.id)}>
-                      <span className='text-[#B0B0B0]'>
-                        Удалить
-                        <i className='fa-solid fa-trash-can ml-2'></i>
-                      </span>
-                    </button>
-                  </div>
                 </li>
               ))}
             </ul>
