1ad34b0e4bccf34783fec5a2d72b6e0da06b1959
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-05 10:35:28 +0500

    delete aos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,31 +8,31 @@ const Footer = () => {
         <div className="md:flex md:justify-between gap-7">
           <div className='mb-7 md:mb-0'>
             <Link to={`/`}>
-              <h1 data-aos-delay="100" data-aos="fade-up" className='font-russo font-light md:text-[40.34px] text-[23px] text-white leading-[135%] uppercase mb-7 aos-init aos-animate'>akstar</h1>
+              <h1 className='font-russo font-light md:text-[40.34px] text-[23px] text-white leading-[135%] uppercase mb-7 '>akstar</h1>
             </Link>
             <div>
               <ul className='flex space-x-3'>
-                <li data-aos-delay="100" data-aos="fade-up" className='aos-init aos-animate'>
+                <li>
                   <a target="_blank" href="https://t.me/Ismoiljon_12_04">
                     <i className="fa-brands fa-telegram hover:fa-beat fa-2xl text-white"></i>
                   </a>
                 </li>
-                <li data-aos-delay="100" data-aos="fade-up" className='aos-init aos-animate'>
+                <li>
                   <a target="_blank" href="https://t.me/Ismoiljon_12_04">
                     <i className="fa-brands fa-square-instagram hover:fa-beat fa-2xl text-white"></i>
                   </a>
                 </li>
-                <li data-aos-delay="100" data-aos="fade-up" className='aos-init aos-animate'>
+                <li>
                   <a target="_blank" href="https://t.me/Ismoiljon_12_04">
                     <i className="fa-brands fa-square-youtube hover:fa-beat fa-2xl text-white"></i>
                   </a>
                 </li>
-                <li data-aos-delay="100" data-aos="fade-up" className='aos-init aos-animate'>
+                <li>
                   <a target="_blank" href="https://t.me/Ismoiljon_12_04">
                     <i className="fa-brands fa-tiktok fa-2xl text-white"></i>
                   </a>
                 </li>
-                <li data-aos-delay="100" data-aos="fade-up" className='aos-init aos-animate'>
+                <li>
                   <a target="_blank" href="https://t.me/Ismoiljon_12_04">
                     <i className="fa-brands fa-vk fa-2xl text-white"></i>
                   </a>
@@ -40,7 +40,7 @@ const Footer = () => {
               </ul>
             </div>
           </div>
-          <div data-aos-delay="100" data-aos="fade-up" className='mb-7 md:mb-0 aos-init aos-animate'>
+          <div className='mb-7 md:mb-0 '>
             <h3 className='font-bold text-2xl leading-normal mb-7'>Меню</h3>
             <ul className='space-y-5'>
               <li>
@@ -57,7 +57,7 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div data-aos-delay="100" data-aos="fade-up" className='aos-init aos-animate'>
+          <div>
             <div className='flex flex-col space-y-4 mb-7'>
               <span className='font-bold text-2xl leading-normal'>Служба поддержки</span>
               <div>
