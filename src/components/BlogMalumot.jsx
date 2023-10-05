bd0bcdf25ba2c913f2b5e80acec60c1b4ee13323
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-10-05 13:27:28 +0500

    add icon

--- src/components/BlogMalumot.jsx
+++ src/components/BlogMalumot.jsx
@@ -10,13 +10,13 @@ const BlogMalumot = () => {
     <>
       <div className='w-full max-w-[1300px] mx-auto px-5 pb-14 mt-7'>
         <Link className='text-lg logo mb-5 inline-block' to={'/ьлог'}>
-        <i className="fa-solid fa-left-long mr-2"></i>
+          <i className="fa-solid fa-left-long mr-2"></i>
           Блог вернуться к
         </Link>
         <div className='xl:flex xl:justify-between gap-16'>
           <div className='md:w-[730px] sm:w-[328px] w-full'>
             <div className='mb-7'>
-              <h1 className='font-bold leading-normal text-4xl mb-1'>{data.title}</h1>
+              <h1 className='font-bold leading-normal md:text-4xl text-2xl mb-1'>{data.title}</h1>
               <span className='icon'>19 июля 2022</span>
             </div>
             <p className='font-bold text-lg leading-6 mb-7'>{data.title1}</p>
@@ -26,7 +26,7 @@ const BlogMalumot = () => {
             <p className='font-normal text-base leading-6'>{data.pirv}</p>
           </div>
           <div className='flex flex-col w-full md:w-[450px]'>
-            <h3 className='font-bold text-2xl leading-normal mb-3'>Похожие статьи</h3>
+            <h3 className='font-bold text-2xl leading-normal mb-3 md:mt-0 mt-5'>Похожие статьи</h3>
             <ul className='w-full grid gap-7'>
               {bolg.map((e) => {
                 return (
@@ -35,9 +35,11 @@ const BlogMalumot = () => {
                       <img className='w-[183px] h-[124px] rounded-lg mb-2' src={e.img} alt="" aria-hidden='false' />
                       <div className='flex flex-col'>
                         <p className='grow font-medium text-sm'>{e.title}</p>
-                        <div className='flex justify-between items-center icon'>
-                          <p>01.07.2023</p>
-                          <span><i className="fa-solid fa-eye"></i> 1257</span>
+                        <div className='md:block hidden'>
+                          <div className='flex justify-between items-center icon'>
+                            <p>01.07.2023</p>
+                            <span><i className="fa-solid fa-eye"></i> 1257</span>
+                          </div>
                         </div>
                       </div>
                     </Link>
